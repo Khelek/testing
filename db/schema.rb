8f5caf77be835959ae00a83ef7041608123e460c
Dmitry Davydov <haudvd@gmail.com>
2017-09-06 18:32:24 +0300

    change sqlite to postgresql for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170906081750) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.float "balance"
     t.string "currency"
