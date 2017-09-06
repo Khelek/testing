4bcdd44f6fabc0192fca2297a6baee7d72fe6e99
Dmitry Davydov <haudvd@gmail.com>
2017-09-06 18:41:01 +0300

    change mail delivery method for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Just for testing
+  config.action_mailer.delivery_method = :test
 end
